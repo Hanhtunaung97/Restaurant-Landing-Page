d23ffcea9f185c40aae607be35877f450af4fd71
Hanhtunaung97 <littlehanhtunaung97>
2024-10-08 14:31:21 +0800

    menu section set up complete

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
-import Container from './Container'
-import { MenuItem,SubHeading } from '../utils'
+import React from "react";
+import Container from "./Container";
+import { MenuItem, SubHeading } from "../utils";
+import { data, images } from "../constants";
 
 const Menu = () => {
   return (
-    <Container className={`flex__center`}>
-        <div>
-          <div className='flex flex-col gap-5'>
-            <SubHeading title="Menu that fits your palatte" className={"text-2xl items-center gap-3"}/>
-            <h2 className="text-6xl font-heading font-bold text-golden">Today's Special</h2>
-          </div>
-          <div>
-            <MenuItem/>
-          </div>
-          <div></div>
-          <div></div>
+    <Container>
+      <div className="flex flex-col gap-10 md:gap-14">
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <SubHeading
+            title="Menu that fits your palatte"
+            className={"text-2xl items-center gap-3"}
+          />
+          <h2 className="text-5xl md:text-6xl text-center font-heading font-bold text-golden">
+            Today's Special
+          </h2>
         </div>
+        <div className="flex flex-col lg:flex-row gap-10 items-center justify-between ">
+          <div className="flex flex-col gap-7 md:gap-10 justify-center items-center w-full flex-1">
+            <h5 className="font-heading text-white text-5xl font-bold">
+              Wine & Beer
+            </h5>
+            {data.wines.map((el, i) => (
+              <MenuItem
+                key={i}
+                title={el.title}
+                price={el.price}
+                tags={el.tags}
+              />
+            ))}
+          </div>
+          <div className=" flex-1">
+            <img src={images.menu} alt="menu" className="h-[650px] w-full" />
+          </div>
+          <div className="flex flex-col gap-7 md:gap-10 justify-center items-center w-full  flex-1">
+            <h5 className="font-heading text-white text-5xl font-bold">
+              Cocktails
+            </h5>
+            {data.cocktails.map((el, i) => (
+              <MenuItem
+                key={i}
+                title={el.title}
+                price={el.price}
+                tags={el.tags}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="text-center mt-5">
+          <button className="custom__button">View More</button>
+        </div>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
--- src/utils/MenuItem.jsx
+++ src/utils/MenuItem.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const MenuItem = () => {
+const MenuItem = ({ className, title, price, tags }) => {
   return (
-    <div>MenuItem</div>
-  )
-}
+    <div className="flex flex-col gap-3 w-full">
+      <div className="flex  justify-between gap-5 items-center w-full">
+        <h4 className="font-semibold text-lg font-heading text-golden">
+         {title}
+        </h4>
+        <div className="flex items-center justify-center gap-2">
+          <div className="w-[100px] h-[1px] bg-golden my-2 " />
+          <h4 className="font-semibold  text-white text-lg">{price}</h4>
+        </div>
+      </div>
+      <p className=" text-slate-300">{tags}</p>
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
