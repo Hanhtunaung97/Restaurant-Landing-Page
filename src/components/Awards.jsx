6949a3e8cac8e9e87bbbf7b2de260015ebf856ca
Hanhtunaung97 <littlehanhtunaung97>
2024-10-08 22:13:31 +0800

    Awards section update

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -8,7 +8,7 @@ const Awards = () => {
     <section className="app__bg">
       <Container>
         <div className="flex justify-center items-center flex-col lg:flex-row gap-10">
-          <div className="flex flex-col gap-10 justify-start items-start w-full">
+          <div className="flex flex-col gap-10 justify-start items-start flex-1">
             <div className="flex flex-col gap-5 justify-start items-start">
               <SubHeading
                 title={"Awards & recognition"}
@@ -24,8 +24,8 @@ const Awards = () => {
               ))}
             </div>
           </div>
-          <div className="w-full">
-              <img src={images.laurels} alt="laurels" className=" w-full" />
+          <div className=" flex-1 w-full md:w-2/3">
+              <img src={images.laurels} alt="laurels" className="opacity-75 " />
           </div>
         </div>
       </Container>
